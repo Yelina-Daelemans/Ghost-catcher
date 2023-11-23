14e84b20ee2f913d8efcb3ec329cff467e40bd37
Yelina-Daelemans <144011374+Yelina-Daelemans@users.noreply.github.com>
2023-11-23 21:17:52 +0100

    Graphics, FullScreen en Resolution
    Vandaag heb ik gewerkt aan de graphics, FullScreen en Resolution van de game dat je kan aan passen in de opties menu. De graphics en FullScreen zijn al volledig af en werken maar de Resolutions ben ik nog mee bezig om het goed te laten werken.

--- Assets/Scripts/Settings.cs
+++ Assets/Scripts/Settings.cs
@@ -1,11 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class Settings : MonoBehaviour
 {
+    public AudioMixer audiomixer;
+    Resolution[] resolutions;
+    public Dropdown resolutionsdropdown;
+
+    void Start()
+    {
+        resolutions = Screen.resolutions;
+        resolutionsdropdown.ClearOptions();
+        List<string> options = new List<string>();
+        for(int i = 0; i < resolutions.Length; i++)
+        {
+            string options = resolutions[i].width + "x" + resolutions[i].height;
+            options.Add(option);
+        }
+        resolutionsdropdown.AddOptions(resolutions);
+    }
     public void SetVolume(float volume)
     {
         Debug.Log(volume);
+    }
+    public void Quality(int QualityIndex)
+    {
+        QualitySettings.SetQualityLevel(QualityIndex);
+    }
+    public void FullScreen(bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
+        if (isFullscreen == true)
+        {
+            Debug.Log("Fullscreen is aan");
+        }
+        else
+        {
+            Debug.Log("Fullscreen is uit");
+        }
     }
 }
